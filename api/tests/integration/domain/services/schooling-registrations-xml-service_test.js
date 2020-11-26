012143d3616dc7ae7016e878dc638ec6f94bfd7b
ajoanny <ajoanny@octo.com>
2020-11-26 11:52:01 +0100

    [API] fix linter add missing ;

--- api/tests/integration/domain/services/schooling-registrations-xml-service_test.js
+++ api/tests/integration/domain/services/schooling-registrations-xml-service_test.js
@@ -176,7 +176,6 @@ describe.only('Integration | Services | schooling-registrations-xml-service', ()
         //then
         expect(result).to.deep.equal(expectedSchoolingRegistrations);
       });
-
-    })
+    });
   });
 });
