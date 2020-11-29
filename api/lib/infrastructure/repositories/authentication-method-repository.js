2b6c849ba292b1a7b61fbf2527c79cebb29a3714
Christophe Charles <christophe.charles@octo.com>
2020-11-29 17:43:38 +0100

    [API] Update authenticationMethod repository methods
    BSR on required files
    Add updateOnlyPassword()
    Add updatePasswordThatShouldBeChanged()
    Add updateExpiredPassword()

--- api/lib/infrastructure/repositories/authentication-method-repository.js
+++ api/lib/infrastructure/repositories/authentication-method-repository.js
@@ -1,8 +1,15 @@
-const BookshelfAuthenticationMethod = require('../../infrastructure/data/authentication-method');
+const Bookshelf = require('../bookshelf');
 const bookshelfUtils = require('../utils/knex-utils');
-const AuthenticationMethod = require('../../domain/models/AuthenticationMethod');
+
 const DomainTransaction = require('../DomainTransaction');
-const { AlreadyExistingEntity, NotFoundError } = require('../../domain/errors');
+const {
+  AlreadyExistingEntityError,
+  AuthenticationMethodNotFoundError,
+} = require('../../domain/errors');
+
+const AuthenticationMethod = require('../../domain/models/AuthenticationMethod');
+
+const BookshelfAuthenticationMethod = require('../../infrastructure/data/authentication-method');
 
 function _toDomainEntity(bookshelfAuthenticationMethod) {
   const attributes = bookshelfAuthenticationMethod.toJSON();
@@ -18,16 +25,93 @@ function _toDomainEntity(bookshelfAuthenticationMethod) {
 }
 module.exports = {
 
-  async create({ authenticationMethod, domainTransaction = DomainTransaction.emptyTransaction() }) {
+  async create({
+    authenticationMethod,
+    domainTransaction = DomainTransaction.emptyTransaction(),
+  }) {
     try {
       const bookshelfAuthenticationMethod = await new BookshelfAuthenticationMethod(authenticationMethod)
         .save(null, { transacting: domainTransaction.knexTransaction });
       return _toDomainEntity(bookshelfAuthenticationMethod);
     } catch (err) {
       if (bookshelfUtils.isUniqConstraintViolated(err)) {
-        throw new AlreadyExistingEntity(`A snapshot already exists for the user ${authenticationMethod.userId} and the externalIdentifier ${authenticationMethod.externalIdentifier}.`);
+        throw new AlreadyExistingEntityError(`A snapshot already exists for the user ID ${authenticationMethod.userId} and the externalIdentifier ${authenticationMethod.externalIdentifier}.`);
       }
     }
+  },
+
+  updateOnlyPassword({ userId, hashedPassword }) {
+    return BookshelfAuthenticationMethod
+      .where({
+        userId,
+        identityProvider: AuthenticationMethod.identityProviders.PIX,
+      })
+      .save(
+        {
+          authenticationComplement: Bookshelf.knex.raw(`jsonb_set("authenticationComplement", '{password}', '"${hashedPassword}"')`),
+        },
+        { patch: true, method: 'update' },
+      )
+      .then(_toDomainEntity)
+      .catch((err) => {
+        if (err instanceof BookshelfAuthenticationMethod.NoRowsUpdatedError) {
+          throw new AuthenticationMethodNotFoundError(`Authentication method PIX for User ID ${userId} not found.`);
+        }
+        throw err;
+      });
+  },
+
+  updatePasswordThatShouldBeChanged({
+    userId,
+    hashedPassword,
+    domainTransaction = DomainTransaction.emptyTransaction(),
+  }) {
+    const authenticationComplement = new AuthenticationMethod.PasswordAuthenticationMethod({
+      password: hashedPassword,
+      shouldChangePassword: true,
+    });
+
+    return BookshelfAuthenticationMethod
+      .where({
+        userId,
+        identityProvider: AuthenticationMethod.identityProviders.PIX,
+      })
+      .save(
+        { authenticationComplement },
+        {
+          transacting: domainTransaction.knexTransaction,
+          patch: true,
+          method: 'update',
+        },
+      )
+      .then(_toDomainEntity)
+      .catch((err) => {
+        if (err instanceof BookshelfAuthenticationMethod.NoRowsUpdatedError) {
+          throw new AuthenticationMethodNotFoundError(`Authentication method PIX for User ID ${userId} not found.`);
+        }
+        throw err;
+      });
+  },
+
+  updateExpiredPassword({ userId, hashedPassword }) {
+    const authenticationComplement = new AuthenticationMethod.PasswordAuthenticationMethod({
+      password: hashedPassword,
+      shouldChangePassword: false,
+    });
+
+    return BookshelfAuthenticationMethod
+      .where({
+        userId,
+        identityProvider: AuthenticationMethod.identityProviders.PIX,
+      })
+      .save({ authenticationComplement }, { patch: true, method: 'update' })
+      .then(_toDomainEntity)
+      .catch((err) => {
+        if (err instanceof BookshelfAuthenticationMethod.NoRowsUpdatedError) {
+          throw new AuthenticationMethodNotFoundError(`Authentication method PIX for User ID ${userId} not found.`);
+        }
+        throw err;
+      });
   },
 
   async findOneByUserIdAndIdentityProvider({ userId, identityProvider }) {
@@ -54,7 +138,7 @@ module.exports = {
       return _toDomainEntity(bookshelfAuthenticationMethod);
     } catch (err) {
       if (err instanceof BookshelfAuthenticationMethod.NoRowsUpdatedError) {
-        throw new NotFoundError(`No rows updated for authentication method of type ${identityProvider} for user ${userId}.`);
+        throw new AuthenticationMethodNotFoundError(`No rows updated for authentication method of type ${identityProvider} for user ${userId}.`);
       }
       throw err;
     }
