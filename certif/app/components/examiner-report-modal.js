5caa4328ee768e2543fe5ccf9a9ae8d3a3fadd00
Anne-G <ang.schall@gmail.com>
2020-11-30 12:32:44 +0100

    [Certif] Add late-or-leaving category choice

--- certif/app/components/examiner-report-modal.js
+++ certif/app/components/examiner-report-modal.js
@@ -2,21 +2,46 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { OTHER, LATE_OR_LEAVING } from 'pix-certif/models/certification-issue-report';
 
 export default class ExaminerReportModal extends Component {
   @service store
 
   reportOfTypeOther = this.args.report.examinerComment;
 
+  constructor() {
+    super(...arguments);
+    this.currentIssueReport = this.store.createRecord('certification-issue-report', { certificationCourseId: this.args.report.certificationCourseId });
+  }
+
   @tracked
-  isReportOfTypeOtherChecked = false
+  isReportOfTypeOtherChecked = false;
+
+  @tracked
+  isReportOfTypeLateOrLeavingChecked = false;
 
   @tracked
   reportLength = 0
 
   @action
-  async toggleShowReportOfTypeOther() {
+  toggleShowReportOfTypeOther() {
     this.isReportOfTypeOtherChecked = !this.isReportOfTypeOtherChecked;
+    if (this.isReportOfTypeOtherChecked) {
+      this.currentIssueReport.categoryId = OTHER;
+      this.isReportOfTypeLateOrLeavingChecked = false;
+    }
+    if (this.args.report.examinerComment) {
+      this.reportLength = this.args.report.examinerComment.length;
+    }
+  }
+
+  @action
+  toggleShowReportOfTypeLateOrLeaving() {
+    this.isReportOfTypeLateOrLeavingChecked = !this.isReportOfTypeLateOrLeavingChecked;
+    if (this.isReportOfTypeOtherChecked) {
+      this.currentIssueReport.categoryId = LATE_OR_LEAVING;
+      this.isReportOfTypeOtherChecked = false;
+    }
     if (this.args.report.examinerComment) {
       this.reportLength = this.args.report.examinerComment.length;
     }
@@ -24,12 +49,12 @@ export default class ExaminerReportModal extends Component {
 
   @action
   submitReport() {
-    this.args.report.examinerComment = this.reportOfTypeOther;
+    this.args.report.certificationIssueReports.push(this.currentIssueReport);
     this.args.closeModal();
   }
 
   @action
-  handleChange(e) {
+  handleTextareaChange(e) {
     this.reportLength = e.target.value.length;
   }
 }
